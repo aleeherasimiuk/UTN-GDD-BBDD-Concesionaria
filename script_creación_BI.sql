e627aee8d8256154d22aac398374f314c698006f
SwaXTech <aleeherasimiuk@gmail.com>
2020-09-14 18:19:11 -0300

    Added empty scripts

---
+++ script_creación_BI.sql
@@ -0,0 +1,8 @@
+USE [GD2C2020]
+GO
+
+/****** Object:  Schema [EMPANADA_DE_MONDONGO]    Script Date: 9/14/2020 6:15:23 PM ******/
+CREATE SCHEMA [EMPANADA_DE_MONDONGO]
+GO
+
+
---
+++ script_creación_inicial.sql
@@ -0,0 +1,9 @@
+USE [GD2C2020]
+GO
+
+/****** Object:  Schema [EMPANADA_DE_MONDONGO]    Script Date: 9/14/2020 6:12:21 PM ******/
+CREATE SCHEMA [EMPANADA_DE_MONDONGO]
+GO
+
+
+SELECT * FROM gd_esquema.Maestra
