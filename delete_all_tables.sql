cca65998521245e1916e8eb722019aecbc57e117
SwaXTech <aleeherasimiuk@gmail.com>
2020-10-17 16:23:23 -0300

    update delete tables script

--- delete_all_tables.sql
+++ delete_all_tables.sql
@@ -3,8 +3,6 @@ GO
 
 SELECT CURRENT_USER;
 
-EXEC dbo.delete_tables;
-
 CREATE FUNCTION dbo.table_exists(@name VARCHAR(100)) RETURNS INT AS
 BEGIN
 	IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = @name) 
@@ -13,6 +11,18 @@ BEGIN
 		   
 END
 GO
+
+IF dbo.table_exists('compra_auto') = 1
+	DROP TABLE EMPANADA_DE_MONDONGO.compra_auto 
+
+IF dbo.table_exists('compra_item') = 1
+	DROP TABLE EMPANADA_DE_MONDONGO.compra_item 
+
+IF dbo.table_exists('factura_auto') = 1
+	DROP TABLE EMPANADA_DE_MONDONGO.factura_auto
+
+IF dbo.table_exists('factura_item') = 1
+	DROP TABLE EMPANADA_DE_MONDONGO.factura_item 
 
 EXEC sp_MSforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT all"
 
@@ -25,23 +35,11 @@ IF dbo.table_exists('autoparte') = 1
 IF dbo.table_exists('compra') = 1
 	DROP TABLE EMPANADA_DE_MONDONGO.compra
 
-IF dbo.table_exists('compra_auto') = 1
-	DROP TABLE EMPANADA_DE_MONDONGO.compra_auto 
-
-IF dbo.table_exists('compra_item') = 1
-	DROP TABLE EMPANADA_DE_MONDONGO.compra_item 
-
 IF dbo.table_exists('fabricante') = 1
 	DROP TABLE EMPANADA_DE_MONDONGO.fabricante
 
 IF dbo.table_exists('factura') = 1
 	DROP TABLE EMPANADA_DE_MONDONGO.factura
-
-IF dbo.table_exists('factura_auto') = 1
-	DROP TABLE EMPANADA_DE_MONDONGO.factura_auto
-
-IF dbo.table_exists('factura_item') = 1
-	DROP TABLE EMPANADA_DE_MONDONGO.factura_item 
 
 IF dbo.table_exists('modelo') = 1
 	DROP TABLE EMPANADA_DE_MONDONGO.modelo
@@ -61,56 +59,26 @@ IF dbo.table_exists('tipo_transmision') = 1
 IF dbo.table_exists('cliente') = 1
 	DROP TABLE EMPANADA_DE_MONDONGO.cliente
 
-/*
-CREATE PROCEDURE dbo.delete_tables_dbo AS
-BEGIN
+DROP FUNCTION EMPANADA_DE_MONDONGO.OBTENER_ID_FABRICANTE
+DROP FUNCTION EMPANADA_DE_MONDONGO.OBTENER_ID_CLIENTE
+DROP FUNCTION EMPANADA_DE_MONDONGO.OBTENER_ID_SUCURSAL
 
-	EXEC sp_MSforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT all"
 
-	IF dbo.table_exists('auto') = 1
-		DROP TABLE dbo.auto 
+DROP PROCEDURE EMPANADA_DE_MONDONGO.migrar_auto;
+DROP PROCEDURE EMPANADA_DE_MONDONGO.migrar_autoparte;
+DROP PROCEDURE EMPANADA_DE_MONDONGO.migrar_cliente;
+DROP PROCEDURE EMPANADA_DE_MONDONGO.migrar_compra;
+DROP PROCEDURE EMPANADA_DE_MONDONGO.migrar_compra_auto;
+DROP PROCEDURE EMPANADA_DE_MONDONGO.migrar_compra_item;
+DROP PROCEDURE EMPANADA_DE_MONDONGO.migrar_fabricante;
+DROP PROCEDURE EMPANADA_DE_MONDONGO.migrar_factura;
+DROP PROCEDURE EMPANADA_DE_MONDONGO.migrar_factura_auto;
+DROP PROCEDURE EMPANADA_DE_MONDONGO.migrar_factura_item;
+DROP PROCEDURE EMPANADA_DE_MONDONGO.migrar_modelo;
+DROP PROCEDURE EMPANADA_DE_MONDONGO.migrar_sucursal;
+DROP PROCEDURE EMPANADA_DE_MONDONGO.migrar_tipo_auto;
+DROP PROCEDURE EMPANADA_DE_MONDONGO.migrar_tipo_caja;
+DROP PROCEDURE EMPANADA_DE_MONDONGO.migrar_tipo_transmision;
 
-	IF dbo.table_exists('autoparte') = 1
-		DROP TABLE dbo.autoparte 
-
-	IF dbo.table_exists('cliente') = 1
-		DROP TABLE dbo.cliente
-
-	IF dbo.table_exists('compra') = 1
-		DROP TABLE dbo.compra
-
-	IF dbo.table_exists('compra_auto') = 1
-		DROP TABLE dbo.compra_auto 
-
-	IF dbo.table_exists('compra_item') = 1
-		DROP TABLE dbo.compra_item 
-
-	IF dbo.table_exists('fabricante') = 1
-		DROP TABLE dbo.fabricante
-
-	IF dbo.table_exists('factura') = 1
-		DROP TABLE dbo.factura
-
-	IF dbo.table_exists('factura_auto') = 1
-		DROP TABLE dbo.factura_auto
-
-	IF dbo.table_exists('factura_item') = 1
-		DROP TABLE dbo.factura_item 
-
-	IF dbo.table_exists('modelo') = 1
-		DROP TABLE dbo.modelo
-
-	IF dbo.table_exists('sucursal') = 1
-		DROP TABLE dbo.sucursal
-
-	IF dbo.table_exists('tipo_auto') = 1
-		DROP TABLE dbo.tipo_auto
-
-	IF dbo.table_exists('tipo_caja') = 1
-		DROP TABLE dbo.tipo_caja
-
-	IF dbo.table_exists('tipo_transmision') = 1
-		DROP TABLE dbo.tipo_transmision
-
-END
-GO*/
+DROP TABLE EMPANADA_DE_MONDONGO.logs;
+DROP PROCEDURE EMPANADA_DE_MONDONGO.insert_log;
